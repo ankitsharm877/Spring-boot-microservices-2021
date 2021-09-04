f999b48277256ac3df079ed76d7248a5ed21e6c1
Ankit Sharma <ankitsharm877@gmail.com>
2021-09-04 20:41:09 +0530

    zuul new changes

--- microservices-v1/currency-conversion-service/src/main/java/com/springboot/v1/currencyconversionservice/CurrencyExchangeProxy.java
+++ microservices-v1/currency-conversion-service/src/main/java/com/springboot/v1/currencyconversionservice/CurrencyExchangeProxy.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.springboot.v1.currencyconversionservice.bean.CurrencyExchange;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
-	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 				@PathVariable String from, 
 				@PathVariable String to);
